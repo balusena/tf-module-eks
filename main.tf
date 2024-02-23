faac94982dc100a6a44e6acfdb43c2ba80297a8f
balusena <balusena12@gmail.com>
2024-02-23 14:58:26 +0000

    roboshop kubernetes files

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_eks_node_group" "nodes" {
   }
 }
 
-resource "aws_iam_openid_connect_provider" "oidc-iam" {
+resource "aws_iam_openid_connect_provider" "cluster" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
   url             = aws_eks_cluster.eks.identity.0.oidc.0.issuer
