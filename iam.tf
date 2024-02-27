fbc6bc88cd9703a0fe4fe0637761044258e0c5b5
balusena <balusena12@gmail.com>
2024-02-27 19:13:08 +0000

    roboshop kubernetes files

--- iam.tf
+++ iam.tf
@@ -116,23 +116,6 @@ resource "aws_iam_role" "sa-role" {
   })
 }
 
-#resource "aws_iam_role" "sa-role" {
-#  name = "eks-${var.env}-ssm-pm-role"
-#  assume_role_policy = jsonencode({
-#    Version = "2012-10-17"
-#    Statement = [
-#      {
-#        Action = "sts:AssumeRole"
-#        Effect = "Allow"
-#        Sid    = ""
-#        Principal = {
-#          Service = "ec2.amazonaws.com"
-#        }
-#      },
-#    ]
-#  })
-#}
-
 #Attaching IAM Policy To IAM Role
 resource "aws_iam_role_policy_attachment" "policy-attach" {
   role       = aws_iam_role.sa-role.name
